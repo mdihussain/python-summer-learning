85e2eab5dd043a163d903fc2a7f75794557a274b
MD I HUSSAIN <134651999+mdihussain@users.noreply.github.com>
2023-05-28 17:34:23 -0400

    Add files via upload

---
+++ day-4.py
@@ -0,0 +1,26 @@
+#Md Hussain
+#day 4
+
+#assign an integer to a variable and print it
+my_integer = 10
+print(my_integer)
+
+#assign a string to a variable and print it, use comment
+my_string = "hello, world!"
+print(my_string)#this will pring the string assigned to "my_string"
+
+#assign a boolean to a variable, print it, use comment
+my_boolean = True
+print(my_boolean) #this should just print "True"
+
+#Assign two integers to a two different variables and add them
+#then assign the result into a new varaible
+x = 5
+z = my_integer + x #result should be 15
+print(z)
+
+#Contatenate two strings and then print them
+string1 = "Cows lay "
+string2 = "eggs"
+string3 = string1 + string2
+print(string3)
